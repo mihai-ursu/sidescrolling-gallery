4a632e0204f026806f87283f0e9f8c75aed2f89a
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-10-31 18:48:18 +0200

    Made Gallery sticky

--- src/components/SidescrollingGallery/SidescrollingGallery.tsx
+++ src/components/SidescrollingGallery/SidescrollingGallery.tsx
@@ -1,28 +1,43 @@
 import data from "data/gallery";
+import useElementSize from "hooks/useElementSize";
 import useWindowSize from "hooks/useWindowSize";
 import ScrollSideways from "../ScrollSideways/ScrollSideways";
 import Card from "./components/Card/Card";
 import styles from "./SidescrollingGallery.module.scss";
 
 const SidescrollingGallery = () => {
-  const { width } = useWindowSize();
+  const { width: windowWidth } = useWindowSize();
+  const [
+    galleryWrapperRef,
+    { width: galleryWrapperWidth, height: galleryWrapperHeight },
+  ] = useElementSize();
 
   return (
     <section className={styles.wrapper}>
-      <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>Our Collection</h1>
-      </div>
-      <ScrollSideways
-        direction="left"
-        initialX={width * 0.75}
-        isEffectActive={true}
+      <div
+        className={styles.galleryWrapperOuter}
+        style={{ height: galleryWrapperWidth }}
       >
-        <div className={styles.galleryWrapper}>
-          {data.map((item) => {
-            return <Card key={item.id} {...item} />;
-          })}
+        <div className={styles.content}>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.title}>Our Collection</h1>
+          </div>
+          <div className={styles.galleryWrapper}>
+            <ScrollSideways
+              direction="left"
+              initialX={windowWidth * 0.8}
+              isEffectActive={true}
+              offset={galleryWrapperWidth}
+            >
+              <div ref={galleryWrapperRef} className={styles.cardsWrapper}>
+                {data.map((item) => {
+                  return <Card key={item.id} {...item} />;
+                })}
+              </div>
+            </ScrollSideways>
+          </div>
         </div>
-      </ScrollSideways>
+      </div>
     </section>
   );
 };
