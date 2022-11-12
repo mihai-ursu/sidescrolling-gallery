43191c9005cda9b37a78e647838124bd63af125b
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-12 17:50:51 +0200

    Added page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,30 @@ import SidescrollingGallery from "components/SidescrollingGallery/SidescrollingG
 import useBackgroundColorStore from "store/useColorStore";
 import styles from "../styles/Home.module.scss";
 import { motion } from "framer-motion";
+import Head from "next/head";
 
 export default function Home() {
   const { backgroundColor } = useBackgroundColorStore();
 
   return (
-    <motion.div
-      className={styles.container}
-      animate={{ backgroundColor: backgroundColor }}
-    >
-      <div className={styles.content}>
-        <h1 className={styles.title}>Pursuit of excellence A family Spirit</h1>
-      </div>
-      <SidescrollingGallery />
-      <div className={styles.spacer} />
-    </motion.div>
+    <>
+      <Head>
+        <title>
+          Sidescrolling Gallery | Next.js + Framer Motion + TypeScript
+        </title>
+      </Head>
+      <motion.div
+        className={styles.container}
+        animate={{ backgroundColor: backgroundColor }}
+      >
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            Pursuit of excellence A family Spirit
+          </h1>
+        </div>
+        <SidescrollingGallery />
+        <div className={styles.spacer} />
+      </motion.div>
+    </>
   );
 }
