f2368dbc23104b335a9c1d1ba9e4ce01e70d870d
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-12 09:49:43 +0200

    fixed backgroundColor error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.scss";
 import { motion } from "framer-motion";
 
 export default function Home() {
-  const { color: backgroundColor } = useBackgroundColorStore();
+  const { backgroundColor } = useBackgroundColorStore();
 
   return (
     <motion.div
